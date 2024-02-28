1a79b64a7a94476e987a61a99d8f1db2c105309e
ani <aniabaishvili1@gmail.com>
2024-02-28 18:59:37 +0400

    add main and change css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
-import Main from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
    return(
     <div className="container">
         <Header/>
-        {/* <Main/> */}
+        <Main/>
 
     </div>
    );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import './Main.css'
+
+function Main(){
+    return(
+       <div className='main'>
+        <h4>Summary</h4>
+        <div className='flex'>
+        <div className='box1'></div>
+        <div className='box2'></div>
+        <div className='box3'></div>
+        <div className='box4'></div>
+        </div>
+        <button>Continue</button>
+       </div>
+    )
+}
+
+export default Main;
